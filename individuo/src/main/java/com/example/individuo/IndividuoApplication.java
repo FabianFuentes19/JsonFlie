772ee6b13fb90759c3550099d3806671184ab2b3
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-11-03 14:16:56 -0600

    se agrego el inicializador

---
+++ individuo/src/main/java/com/example/individuo/IndividuoApplication.java
@@ -0,0 +1,13 @@
+package com.example.individuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividuoApplication.class, args);
+	}
+
+}
